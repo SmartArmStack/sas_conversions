b9fdffe197b171a9094341809da562f96a44493e
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2020-06-17 14:39:42 +0900

    Updating header

--- include/rosilo_conversions/rosilo_conversions.h
+++ include/rosilo_conversions/rosilo_conversions.h
@@ -1,7 +1,5 @@
-#ifndef MY_ROS_CONVERSIONS_HEADER_GUARD
-#define MY_ROS_CONVERSIONS_HEADER_GUARD
-#include<my_ros_conversions/DQ_geometry_msgs_conversions.hpp>
-#include<my_ros_conversions/std_std_msgs_conversions.hpp>
-#include<my_ros_conversions/eigen3_std_conversions.hpp>
-#include<my_ros_conversions/math_conversions.hpp>
-#endif
+#pragma once
+#include<rosilo_conversions/DQ_geometry_msgs_conversions.h>
+#include<rosilo_conversions/std_std_msgs_conversions.h>
+#include<rosilo_conversions/eigen3_std_conversions.h>
+
