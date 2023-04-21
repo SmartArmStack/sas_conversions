b8cb41373cbc99ed376ce62733406b1295fed114
Murilo M. Marinho <murilomarinho@ieee.org>
2023-04-21 17:18:39 +0900

    Adding python package-related files

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'sas_conversions'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['sas_conversions'],
-    package_dir={'': 'src'},
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='murilo',
+    maintainer_email='murilomarinho@ieee.org',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    #entry_points={
+    #    'console_scripts': [
+    #    ],
+    },
 )
-
-setup(**setup_args)
